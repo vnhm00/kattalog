dccdffc51f8039c78a943fc36fa3ce39857850d2
Mai <im.maivu@gmail.com>
2026-04-09 23:36:30 +0200

    Add backend application

---
+++ backend/adoptions/resources/center_resource.py
@@ -0,0 +1,30 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+
+from adoptions.models import Center
+
+
+@require_GET
+def centers(request):
+    centers = Center.objects.all()
+    result = []
+
+    for center in centers:
+        result.append(
+            {
+                "id": center.id,
+                "website_url": center.website_url,
+                "slug": center.slug,
+                "name": center.name,
+                "county": center.county,
+                "address": center.address,
+                "logo_url": center.logo_url,
+                "cats": list(center.cats.values()),
+            }
+        )
+
+    return JsonResponse(
+        {
+            "centers": result,
+        }
+    )
