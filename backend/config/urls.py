2a2ff5532e68fd2a9ae0e7fec52f27909b8c7bac
Mai <im.maivu@gmail.com>
2026-04-10 00:09:45 +0200

    Adjust config so deployment works

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def health(_request):
+    return HttpResponse("ok")
+
+
 urlpatterns = [
+    path("health/", health, name="health"),
     path('admin/', admin.site.urls),
     path('', include('adoptions.resources.urls')),
 ]
